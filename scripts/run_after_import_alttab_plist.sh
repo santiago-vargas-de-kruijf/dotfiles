bc3a4a19bb29f0d4eec468aee74ce4a7566b8ed7
Santiago Vargas de Kruijf <santiago.vargas-de-kruijf@pix4d.com>
2025-03-26 10:35:48 +0100

    chore: update alttab scripts

--- scripts/run_after_import_alttab_plist.sh
+++ scripts/run_after_import_alttab_plist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOMAIN="com.lwouis.alt-tab-macos"
-PLIST_PATH="$HOME/.config/$DOMAIN.plist"
+PLIST_PATH="$HOME/.config/alttab/$DOMAIN.plist"
 
 echo "Importing XML changes into $DOMAIN..."
 defaults import "$DOMAIN" "$PLIST_PATH"
