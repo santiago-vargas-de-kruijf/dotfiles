23763e0a270aea5480d17f9246aea97548df9761
Santiago Vargas de Kruijf <santiago.vargas-de-kruijf@pix4d.com>
2025-03-26 11:48:25 +0100

    chore: change alttab default appereance to test again

--- scripts/export-alttab-plist.sh
+++ scripts/export-alttab-plist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOMAIN="com.lwouis.alt-tab-macos"
-PLIST_PATH="$(chezmoi source-path)/alttab/$DOMAIN.plist"
+PLIST_PATH="$(chezmoi source-path)/dot_config/alttab/$DOMAIN.plist"
 
 echo "Exporting $DOMAIN to XML for chezmoi tracking..."
 defaults export "$DOMAIN" "$PLIST_PATH"
