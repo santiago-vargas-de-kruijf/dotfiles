7a318ec115065efd8f157652642f82581e3c7369
Santiago Vargas de Kruijf <santiago.vargas-de-kruijf@pix4d.com>
2025-03-26 10:54:38 +0100

    chore: add before test script with other name

--- src/modify_com.googlecode.iterm2.plist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PLIST="$HOME/.config/iterm2/com.googlecode.iterm2.plist"
-
-# Backup before modification
-cp "$PLIST" "PLIST.bak"
-
-# Remove specific keys using 'plutil'
-plutil -remove "Window Arrangements" "$PLIST"
-plutil -remove "Default Arrangement Name" "$PLIST"
--- src/run_before_export-alttab-plist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DOMAIN="com.lwouis.alt-tab-macos"
-PLIST_PATH="$HOME/.config/alttab/$DOMAIN.plist"
-
-echo "Exporting $DOMAIN to XML for chezmoi tracking..."
-defaults export "$DOMAIN" "$PLIST_PATH"
