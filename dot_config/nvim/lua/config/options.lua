eaf99b55a1b9ff28c2a797e13bfa9ac885d1d778
Santiago Vargas de Kruijf <santiago.vargas-de-kruijf@pix4d.com>
2025-03-28 08:31:42 +0100

    chore: update configs

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -5,3 +5,21 @@ vim.opt.relativenumber = false
 vim.opt.wrap = true
 vim.opt.timeoutlen = 300
 vim.g.material_style = "lighter"
+vim.diagnostic.config({
+  -- Use the default configuration
+  virtual_lines = true,
+
+  -- Alternatively, customize specific options
+  -- virtual_lines = {
+  --  -- Only show virtual line diagnostics for the current cursor line
+  --  current_line = true,
+  -- },
+})
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method("textDocument/completion") then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
