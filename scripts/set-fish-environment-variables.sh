7ffd9e2933c8d02a8763bbe656b1c4afcf5f260e
Santiago Vargas de Kruijf <santiago.vargas-de-kruijf@pix4d.com>
2025-03-28 11:11:21 +0100

    chore: add volta

--- scripts/set-fish-environment-variables.sh
+++ scripts/set-fish-environment-variables.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo "Remeber to use fish SCRIPT.sh"
+echo "Remember to use fish SCRIPT.sh"
+
+echo "Setting $EDITOR"
+set -gx EDITOR hx
+
 echo "Setting EZA config directory"
 set -Ux EZA_CONFIG_DIR $HOME/.config/eza/
+
+echo "Setting Volta directory"
+set -Ux VOLTA_HOME $HOME/.volta
---
+++ scripts/set-fish-paths.sh
@@ -0,0 +1,3 @@
+echo "Setting fish paths"
+
+fish_add_path $VOLTA_HOME/bin
